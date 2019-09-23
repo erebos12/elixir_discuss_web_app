49bc7eb8949c61da77f95664021648fe919eed20
Alex Sahm <sahm.alexander@pwc.com>
2019-09-23 09:33:38 +0200

    added user insert/update for OAuth

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -2,9 +2,20 @@ defmodule Discuss.AuthController do
   use Discuss.Web, :controller
   plug Ueberauth
 
-  def callback(conn, params) do
-    IO.inspect(conn.assigns)
-    IO.inspect(params)
+  alias Discuss.User
+
+  def callback(%{assigns: %{ ueberauth_auth: auth}} = conn, params) do
+    IO.inspect(auth)
+    user_params = %{ token: auth.credentials.token, email: auth.info.email, provider: "github" }
+    changeset = User.changeset(%User{}, user_params)
+    insert_or_update_user(changeset)
+  end
+
+  defp insert_or_update_user(changeset) do
+     case Repo.get_by(User, email: changeset.changes.email) do
+      nil -> Repo.insert(changeset) # user is not yet existing,s o insert it
+      user -> {:ok, user}
+     end
   end
 
 end
